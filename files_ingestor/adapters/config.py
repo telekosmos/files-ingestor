e13cf78268bfc8aff9c81a784ad16311969afca9
Guillermo C. Martínez <telekosmos@ymail.com>
2025-03-05 13:27:30 +0100

    feat: add dotenv

--- files_ingestor/adapters/config.py
+++ files_ingestor/adapters/config.py
@@ -1,9 +1,11 @@
 from typing import Any, Optional
 
 import config
+import dotenv
 
 from files_ingestor.domain.ports.config import ConfigPort
 
+dotenv.load_dotenv()
 
 class ConfigConfig(ConfigPort):
     def __init__(self, file_path: str | None = "config.json") -> None:
@@ -11,7 +13,7 @@ class ConfigConfig(ConfigPort):
         self.cfg = config.Config(file_path)
 
     def get(self, key: str, default: Optional[Any] = None) -> Any:
-        try:    
+        try:
             return self.cfg[key]
         except config.ConfigError as ce:
             if default is not None:
