6322f0a07cda107b4b6df7369648605863b3ae53
Guillermo C. Martínez <telekosmos@ymail.com>
2025-03-05 13:29:29 +0100

    feat: added config to terminal main

--- files_ingestor/main_terminal.py
+++ files_ingestor/main_terminal.py
@@ -1,20 +1,24 @@
 import logging
 
+from files_ingestor.adapters.config import ConfigConfig
 from files_ingestor.adapters.default_logger import DefaultLoggerAdapter
 from files_ingestor.adapters.repositories.file_reader import FileReaderAdapter
 from files_ingestor.adapters.terminal import TerminalAdapter
 from files_ingestor.application.handlers.count_file_handler import CountFileHandler
+from files_ingestor.domain.ports.config import ConfigPort
 from files_ingestor.domain.ports.logger_port import LoggerPort
 from files_ingestor.domain.services.file_processor_service import FileProcessorService
 
 
 def main():
-    logger: LoggerPort = DefaultLoggerAdapter(log_level=logging.DEBUG) 
+    logger: LoggerPort = DefaultLoggerAdapter(log_level=logging.DEBUG)
+    config: ConfigPort = ConfigConfig()
+    print(f"from config {config.get("llm.anthropic.name")}")
     # Instantiate the FileReaderAdapter (driven adapter)
     file_reader_adapter = FileReaderAdapter()
 
     # Instantiate the FileProcessorService (business logic)
-    file_processor_service = FileProcessorService(logger, file_reader_adapter)
+    file_processor_service = FileProcessorService(logger, None, None, file_reader_adapter)
 
     # Instantiate the Query Handler
     count_file_handler = CountFileHandler(file_processor_service)
