6d4f72663cb13f21e59ecc47db082dd538250143
Guillermo C. Martínez <telekosmos@ymail.com>
2025-03-13 20:56:58 +0100

    chore: tests updated

--- tests/test_file_processor_service.py
+++ tests/test_file_processor_service.py
@@ -6,6 +6,7 @@ import unittest
 from torch import embedding
 from files_ingestor.adapters.llms.anthropic import AnthropicAdapter as AnthropicLLM
 from files_ingestor.adapters.llms.ollama import OllamaAdapter as OllamaLLM
+from files_ingestor.domain.ports.config import ConfigPort
 from files_ingestor.domain.ports.embedding_model import EmbeddingModelPort
 from files_ingestor.domain.ports.llm import FunctionCallingLLMPort
 from files_ingestor.domain.ports.vectorstore import VectorStorePort
@@ -28,8 +29,10 @@ class TestFileProcessorService(unittest.TestCase):
         self.file_reader = Mock()
         self.mock_vector_store = Mock(spec=VectorStorePort)
         self.mock_embeddings = Mock(spec=EmbeddingModelPort)
+        self.mock_config = Mock(spec=ConfigPort)
         self.service = FileProcessorService(self.logger,
-                                            self.mock_vector_store, 
+                                            self.mock_config,
+                                            self.mock_vector_store,
                                             self.mock_embeddings,
                                             self.file_reader)
 
@@ -100,21 +103,52 @@ class TestIngestorService(unittest.TestCase):
     def setUp(self):
         self.logger = NullLoggerAdapter()
         self.file_reader = Mock()
+        self.config = Mock(spec=ConfigPort)
         # self.service = FileProcessorService(self.logger, self.file_reader)
 
     def tearDown(self):
         return super().tearDown()
 
-    def zzz_test_pdf_ingestion(self):
-        qdrant_server = os.getenv("QDRANT_SERVER")
-        qdrant_repo = QdrantRepository(qdrant_server, self.logger)
-        pdf_filepath = "~/Workon/data/aws-overview.pdf"
-        service = FileProcessorService(self.logger,
-                                       vector_store_repo=qdrant_repo,
-                                       embeddings_port=OllamaEmbeddingModel(),
-                                       file_reader=self.file_reader)
-        nodes = service.ingest_pdf(pdf_filepath)
-        self.assertGreater(len(nodes), 0)
+    # def zzz_test_pdf_ingestion(self):
+    #     qdrant_server = os.getenv("QDRANT_SERVER")
+    #     qdrant_repo = QdrantRepository(qdrant_server, self.logger)
+    #     pdf_filepath = "~/Workon/data/aws-overview.pdf"
+    #     service = FileProcessorService(self.logger,
+    #                                    self.mock_config,
+    #                                    vector_store_repo=qdrant_repo,
+    #                                    embeddings_port=OllamaEmbeddingModel(),
+    #                                    file_reader=self.file_reader)
+    #     nodes = service.ingest_pdf(pdf_filepath)
+    #     self.assertGreater(len(nodes), 0)
+
+    def zzz_test_ingest_folder(self):
+        # Arrange
+        folder_path = "test_folder"
+        pdf_file_1 = os.path.join(folder_path, "file1.pdf")
+        pdf_file_2 = os.path.join(folder_path, "file2.txt")  # Non-pdf file to be skipped
+
+        os.makedirs(folder_path, exist_ok=True)
+        with open(pdf_file_1, 'w') as f:
+            f.write("Sample PDF content")
+        with open(pdf_file_2, 'w') as f:
+            f.write("Non-PDF content")
+
+        mock_vector_store = Mock(spec=VectorStorePort)
+        mock_embeddings = Mock(spec=EmbeddingModelPort)
+
+        service = FileProcessorService(self.logger, self.config,
+                                        vector_store_repo=mock_vector_store,
+                                        embeddings_port=mock_embeddings,
+                                        file_reader=self.file_reader)
+
+        # Act
+        num_files = service.ingest_folder(folder_path)
+
+        # Assert
+        self.assertEqual(num_files, 1)  # Only one PDF file should be ingested
+        os.remove(pdf_file_1)
+        os.remove(pdf_file_2)
+        os.rmdir(folder_path)
 
 if __name__ == "__main__":
     unittest.main()
--- tests/test_http_adapter.py
+++ tests/test_http_adapter.py
@@ -1,13 +1,15 @@
+import os
 import unittest
+import unittest.mock
 from unittest import mock
 from unittest.mock import MagicMock, Mock, patch
-import unittest.mock
 
 from fastapi.testclient import TestClient
 
 from files_ingestor.adapters.http import create_http_app
 from files_ingestor.application.handlers.count_file_handler import CountFileHandler
 from files_ingestor.application.handlers.ingestion_handler import IngestionHandler
+from files_ingestor.application.handlers.qa_handler import QAHandler
 from files_ingestor.application.queries.count_file_query import CountFileQuery
 from files_ingestor.domain.ports.file_processor_port import FileProcessorPort
 from files_ingestor.domain.ports.logger_port import LoggerPort
@@ -29,6 +31,42 @@ class TestHttpAdapter(unittest.TestCase):
         response = self.client.get("/status")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {"status": "ok"})
+
+    def test_ingest_folder_endpoint(self):
+        # Setup mock objects
+        logger_mock = MagicMock(spec=LoggerPort)
+        qa_handler_mock = MagicMock(spec=QAHandler)
+        ingestion_handler_mock = MagicMock(spec=IngestionHandler)
+        ingestion_handler_mock.handle.return_value = 1  # Mock the return value of handle method
+
+
+        # Create an instance of the FastAPI app
+        app = create_http_app(logger=logger_mock, query_handler=qa_handler_mock, ingestor_handler=ingestion_handler_mock)
+        client = TestClient(app)
+
+        # Define test folder and its contents
+        test_folder_path = "./tmp/test_folder"
+        os.makedirs(test_folder_path, exist_ok=True)
+        with open(os.path.join(test_folder_path, "test_file.pdf"), "w") as f:
+            f.write("Test file content")
+
+        # Make a POST request to the /ingest-folder endpoint
+        response = client.post("/ingest-folder", json={"folder_path": test_folder_path}, params={"folder_path": test_folder_path})
+
+        # Assert the response status code and content
+        assert response.status_code == 200
+        assert "status" in response.json()
+        assert response.json()["status"] == "success"
+        assert "num_files" in response.json()
+        assert response.json()["num_files"] == 1
+
+        # Clean up test folder
+        for file_name in os.listdir(test_folder_path):
+            file_path = os.path.join(test_folder_path, file_name)
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+        os.rmdir(test_folder_path)
+
 
 if __name__ == "__main__":
     unittest.main()
