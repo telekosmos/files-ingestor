e90b1ecc0b4beb28560b7c9ac4695146876129e9
Guillermo C. Martínez <telekosmos@ymail.com>
2025-04-19 17:09:27 +0200

    feat: added config adapter

--- files_ingestor/adapters/config.py
+++ files_ingestor/adapters/config.py
@@ -7,20 +7,17 @@ from files_ingestor.domain.ports.config import ConfigPort
 
 dotenv.load_dotenv()
 
+
 class ConfigConfig(ConfigPort):
     def __init__(self, file_path: str | None = "config.json") -> None:
         self.file_path = file_path
-        self.cfg = config.Config(file_path)
+        self.cfg = config.Config(file_path)  # type: ignore  # noqa: PGH003
 
     def get(self, key: str, default: Optional[Any] = None) -> Any:
         try:
             return self.cfg[key]
-        except config.ConfigError as ce:
+        except config.ConfigError as ce:  # type: ignore
             if default is not None:
                 return default
             else:
                 raise ce
-
-
-
-
